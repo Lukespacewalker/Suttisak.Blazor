8aa2a78545ef6a4aff035945dd06ffadedaccf1b
Suttisak Denduangchai <17828352+Lukespacewalker@users.noreply.github.com>
2025-12-08 17:07:01 +0700

    Refactor Razor components for extensibility
    Modified access modifiers of properties, methods, and nested classes in various `.razor` files to `protected` or `protected virtual` to enable easier extensibility and overrides in derived classes.
    
    - Updated `Input` properties and `InputModel` classes to `protected` or `protected virtual` in multiple components.
    - Changed methods like `OnValidSubmitAsync`, `OnSubmitAsync`, and others to `protected virtual` for customization.
    - Renamed `LinkLoginCallbackAction` to `LoginCallbackAction` in `ExternalLogins.razor`.
    - Removed unused imports in `_Imports.razor` for cleaner code.
    
    These changes enhance the maintainability and flexibility of the codebase, allowing developers to extend and customize components more effectively.

--- Suttisak.Blazor.Identity/ServiceCollectionExtensions.cs
+++ Suttisak.Blazor.Identity/ServiceCollectionExtensions.cs
@@ -4,13 +4,13 @@ namespace Suttisak.Blazor.Identity;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddBlazorIdentity<TApplicationUser>(
-        this IServiceCollection services) where TApplicationUser : class
+    public static IServiceCollection AddBlazorIdentity<TUser>(
+        this IServiceCollection services) where TUser : class
     {
-        services.AddScoped<IdentityRedirectManager<TApplicationUser>>();
+        services.AddScoped<IdentityRedirectManager<TUser>>();
         services.AddScoped<IdentityRedirectManager>(serviceProvider =>
-            serviceProvider.GetRequiredService<IdentityRedirectManager<TApplicationUser>>());
-        services.AddScoped<IdentityUserAccessor<TApplicationUser>>();
+            serviceProvider.GetRequiredService<IdentityRedirectManager<TUser>>());
+        services.AddScoped<IdentityUserAccessor<TUser>>();
         return services;
     }
 }
