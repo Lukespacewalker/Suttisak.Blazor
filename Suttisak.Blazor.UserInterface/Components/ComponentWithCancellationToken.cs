1eebc8b1bc98edf0aae1aee5e566b140ad28ed51
Lukespacewalker <17828352+Lukespacewalker@users.noreply.github.com>
2026-06-05 05:59:39 +0700

    Add ComponentWithCancellationToken base class
    Introduced an abstract ComponentWithCancellationToken class to manage cancellation tokens and disposal for Blazor components. Updated project version to 0.2.25.

---
+++ Suttisak.Blazor.UserInterface/Components/ComponentWithCancellationToken.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Suttisak.Blazor.UserInterface.Components;
+public abstract class ComponentWithCancellationToken : ComponentBase, IDisposable, IAsyncDisposable
+{
+    private CancellationTokenSource? _cancellationTokenSource;
+
+    protected CancellationToken CtxWhenComponentDetached => (_cancellationTokenSource ??= new CancellationTokenSource()).Token;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing is false) return;
+        _cancellationTokenSource?.Cancel();
+        _cancellationTokenSource?.Dispose();
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual ValueTask DisposeAsyncCore()
+    {
+        _cancellationTokenSource?.Cancel();
+        _cancellationTokenSource?.Dispose();
+        return ValueTask.CompletedTask;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await DisposeAsyncCore();
+        GC.SuppressFinalize(this);
+    }
+}
