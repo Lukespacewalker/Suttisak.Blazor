021961b23421d73b038f04b0af73b9567a62ceae
Lukespacewalker <suttisak.lukesp@outlook.com>
2026-08-12 16:06:02 +0700

    Release UserInterface 0.12.4

--- Suttisak.Blazor.UserInterface/Providers/BrowserTimeProvider.Extension.cs
+++ Suttisak.Blazor.UserInterface/Providers/BrowserTimeProvider.Extension.cs
@@ -1,28 +1,57 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Suttisak.Blazor.UserInterface.Providers;
+
 public static class TimeProviderExtensions
 {
     public static DateTime ToLocalDateTime(this TimeProvider timeProvider, DateTime dateTime)
     {
-        return dateTime.Kind switch
+        var utc = dateTime.Kind switch
         {
             DateTimeKind.Unspecified => throw new InvalidOperationException("Unable to convert unspecified DateTime to local time"),
-            DateTimeKind.Local => dateTime,
-            _ => DateTime.SpecifyKind(TimeZoneInfo.ConvertTimeFromUtc(dateTime, timeProvider.LocalTimeZone), DateTimeKind.Local),
+            DateTimeKind.Local => dateTime.ToUniversalTime(),
+            _ => dateTime,
         };
+        return DateTime.SpecifyKind(
+            TimeZoneInfo.ConvertTimeFromUtc(utc, timeProvider.LocalTimeZone),
+            DateTimeKind.Unspecified);
     }
 
     public static DateTime ToLocalDateTime(this TimeProvider timeProvider, DateTimeOffset dateTime)
     {
-        var local = TimeZoneInfo.ConvertTimeFromUtc(dateTime.UtcDateTime, timeProvider.LocalTimeZone);
-        local = DateTime.SpecifyKind(local, DateTimeKind.Local);
-        return local;
+        return DateTime.SpecifyKind(
+            TimeZoneInfo.ConvertTimeFromUtc(dateTime.UtcDateTime, timeProvider.LocalTimeZone),
+            DateTimeKind.Unspecified);
+    }
+
+    public static DateTimeOffset ToLocalDateTimeOffset(this TimeProvider timeProvider, DateTimeOffset dateTime)
+    {
+        return TimeZoneInfo.ConvertTime(dateTime.ToUniversalTime(), timeProvider.LocalTimeZone);
+    }
+
+    public static DateTimeOffset ToUtcDateTimeOffset(this TimeProvider timeProvider, DateTime localDateTime)
+    {
+        var unspecified = DateTime.SpecifyKind(localDateTime, DateTimeKind.Unspecified);
+        if (timeProvider.LocalTimeZone.IsInvalidTime(unspecified))
+        {
+            throw new ArgumentException("The selected local time does not exist in the browser time zone.", nameof(localDateTime));
+        }
+
+        if (timeProvider.LocalTimeZone.IsAmbiguousTime(unspecified))
+        {
+            throw new ArgumentException("The selected local time is ambiguous in the browser time zone.", nameof(localDateTime));
+        }
+
+        return new DateTimeOffset(TimeZoneInfo.ConvertTimeToUtc(unspecified, timeProvider.LocalTimeZone));
     }
 }
 
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddBrowserTimeProvider(this IServiceCollection services)
-        => services.AddKeyedScoped<TimeProvider, BrowserTimeProvider>(nameof(BrowserTimeProvider));
+    {
+        services.TryAddKeyedScoped<TimeProvider, BrowserTimeProvider>(nameof(BrowserTimeProvider));
+        return services;
+    }
 }
--- Suttisak.Blazor.UserInterface/Providers/BrowserTimeProvider.cs
+++ Suttisak.Blazor.UserInterface/Providers/BrowserTimeProvider.cs
@@ -1,34 +1,58 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Suttisak.Blazor.UserInterface.Providers;
 
 public sealed class BrowserTimeProvider : TimeProvider
 {
     private TimeZoneInfo? _browserLocalTimeZone;
+    private bool _initializationInProgress;
 
-    // Notify when the local time zone changes
     public event EventHandler? LocalTimeZoneChanged;
 
-    public override TimeZoneInfo LocalTimeZone
-        => _browserLocalTimeZone ?? base.LocalTimeZone;
+    // UTC is the safe prerendering fallback. Using the server's local zone would
+    // make the rendered value change for reasons unrelated to the browser.
+    public override TimeZoneInfo LocalTimeZone => _browserLocalTimeZone ?? TimeZoneInfo.Utc;
 
-    internal bool IsLocalTimeZoneSet => _browserLocalTimeZone != null;
+    public bool IsLocalTimeZoneSet => _browserLocalTimeZone is not null;
 
-    // Set the local time zone
-    public void SetBrowserTimeZone(string timeZone)
+    internal bool TryBeginInitialization()
     {
-        if (!TimeZoneInfo.TryFindSystemTimeZoneById(timeZone, out var timeZoneInfo))
+        if (IsLocalTimeZoneSet || _initializationInProgress) return false;
+        _initializationInProgress = true;
+        return true;
+    }
+
+    internal void CompleteInitialization() => _initializationInProgress = false;
+
+    public bool SetBrowserTimeZone(string? timeZoneId)
+    {
+        if (string.IsNullOrWhiteSpace(timeZoneId)
+            || !TryFindTimeZone(timeZoneId, out var timeZoneInfo))
         {
-            timeZoneInfo = null;
+            return false;
         }
 
-        if (!LocalTimeZone.Equals(timeZoneInfo))
+        if (_browserLocalTimeZone?.Equals(timeZoneInfo) is not true)
         {
             _browserLocalTimeZone = timeZoneInfo;
             LocalTimeZoneChanged?.Invoke(this, EventArgs.Empty);
         }
+
+        return true;
+    }
+
+    private static bool TryFindTimeZone(string timeZoneId, out TimeZoneInfo timeZoneInfo)
+    {
+        if (TimeZoneInfo.TryFindSystemTimeZoneById(timeZoneId, out timeZoneInfo!))
+        {
+            return true;
+        }
+
+        if (TimeZoneInfo.TryConvertIanaIdToWindowsId(timeZoneId, out var windowsId)
+            && TimeZoneInfo.TryFindSystemTimeZoneById(windowsId, out timeZoneInfo!))
+        {
+            return true;
+        }
+
+        timeZoneInfo = null!;
+        return false;
     }
 }
