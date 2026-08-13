065f7def495bdffec899e84ffee71d4adf7698e8
Lukespacewalker <suttisak.lukesp@outlook.com>
2026-08-13 17:16:25 +0700

    chore: update framework dependencies and formatting

--- Suttisak.Blazor.UserInterface/Layouts/CommonSections.cs
+++ Suttisak.Blazor.UserInterface/Layouts/CommonSections.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Suttisak.Blazor.UserInterface.Layouts;
+
 public static class CommonSections
 {
     public const string PageTitle = "PageTitle";
