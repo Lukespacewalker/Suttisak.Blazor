7cccbce9b23efb6d1cb0b7e07ebc4bb288b5c6b9
Suttisak Denduangchai <17828352+Lukespacewalker@users.noreply.github.com>
2026-08-23 09:43:53 +0700

    Register Playbook HTTP client for packaged manifests

--- Suttisak.Blazor.Playbook/Program.cs
+++ Suttisak.Blazor.Playbook/Program.cs
@@ -6,6 +6,7 @@ using Suttisak.Blazor.UserInterface.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<PlaybookState>();
 builder.Services.AddSingleton<DemoRecordStore>();
 builder.Services.AddBlazorUserInterface(_ => { });
